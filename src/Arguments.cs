fdd31d28b8e362b199fe6e0fd9cf132ac5956b84
Ashton Moore <ashton.chris.moore@gmail.com>
2023-01-25 21:24:30 +1300

    Use filescoped namespace

--- src/Arguments.cs
+++ src/Arguments.cs
@@ -1,76 +1,75 @@
-namespace Arguments
+namespace Arguments;
+
+public class Argument
 {
-  public class Argument
-  {
-    public List<string> aliases = new List<string>();
-    public string name = String.Empty;
-    public string description = String.Empty;
+  public List<string> aliases = new List<string>();
+  public string name = String.Empty;
+  public string description = String.Empty;
 
-    public Argument(List<string> aliases, string name, string description)
-    {
-      this.aliases = aliases;
-      this.name = name;
-      this.description = description;
-    }
+  public Argument(List<string> aliases, string name, string description)
+  {
+    this.aliases = aliases;
+    this.name = name;
+    this.description = description;
+  }
+}
+
+class ProgramArguments
+{
+  public List<Argument> arguments = new List<Argument>();
+
+  public void AddArgument(List<string> aliases, string name, string description)
+  {
+    arguments.Add(new Argument(aliases, name, description));
   }
 
-  class ProgramArguments
+  public void ShowHelp()
   {
-    public List<Argument> arguments = new List<Argument>();
-
-    public void AddArgument(List<string> aliases, string name, string description)
+    Console.WriteLine();
+    Console.WriteLine("spotty-tools-cli");
+    Console.WriteLine();
+    Console.Write("* Command *");
+    Console.Write("\t");
+    Console.Write("* Name *");
+    Console.Write("\t");
+    Console.WriteLine("* Description *");
+    foreach (Argument arg in this.arguments)
     {
-      arguments.Add(new Argument(aliases, name, description));
-    }
-
-    public void ShowHelp()
-    {
+      Console.Write(arg.aliases[0]);
+      Console.Write("\t\t");
+      Console.Write(arg.name);
+      Console.Write("\t\t");
+      Console.Write(arg.description);
       Console.WriteLine();
-      Console.WriteLine("spotty-tools-cli");
-      Console.WriteLine();
-      Console.Write("* Command *");
-      Console.Write("\t");
-      Console.Write("* Name *");
-      Console.Write("\t");
-      Console.WriteLine("* Description *");
-      foreach (Argument arg in this.arguments)
-      {
-        Console.Write(arg.aliases[0]);
-        Console.Write("\t\t");
-        Console.Write(arg.name);
-        Console.Write("\t\t");
-        Console.Write(arg.description);
-        Console.WriteLine();
-      }
     }
   }
+}
 
-  class ArgumentParser
+class ArgumentParser
+{
+  private List<string> args;
+
+  public ArgumentParser(string[] args)
   {
-    private List<string> args;
+    this.args = args.ToList();
+  }
 
-    public ArgumentParser(string[] args)
+  public string NextArg()
+  {
+    // Args should have at least length 1
+    if (this.args.Count() == 0)
     {
-      this.args = args.ToList();
+      throw new ArgumentsLengthException();
     }
 
-    public string NextArg()
-    {
-      // Args should have at least length 1
-      if (this.args.Count() == 0)
-      {
-        throw new ArgumentsLengthException();
-      }
+    string arg = this.args[0];
+    this.args.RemoveAt(0);
 
-      string arg = this.args[0];
-      this.args.RemoveAt(0);
+    return arg;
+  }
 
-      return arg;
-    }
-
-    public List<string> GetArgs()
-    {
-      return this.args;
-    }
+  public List<string> GetArgs()
+  {
+    return this.args;
   }
 }
