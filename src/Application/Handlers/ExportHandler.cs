f74ff471a87a93789f88e6413d00cd37c3209183
Ashton Moore <ashton.chris.moore@gmail.com>
2023-01-30 21:30:06 +1300

    Add `playlist` command to dispatcher

---
+++ src/Application/Handlers/ExportHandler.cs
@@ -0,0 +1,23 @@
+using Application.CLI.Arguments;
+
+namespace Application.Handlers;
+
+public class ExportHandler
+{
+  public static async Task<int> Dispatch(Application.Spotify.Client client, ArgumentParser argParser)
+  {
+    var nextArg = argParser.NextArg();
+
+    switch (nextArg)
+    {
+      case "playlist":
+        {
+          var playlistName = argParser.NextArg();
+          await client.ExportPlaylist(playlistName);
+          return 0;
+        }
+    }
+
+    return 0;
+  }
+}
