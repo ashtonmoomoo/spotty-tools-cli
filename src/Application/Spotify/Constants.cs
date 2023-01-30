1b274ee3013e11f7ad3265772ec1c1b2c33b87b7
Ashton Moore <ashton.chris.moore@gmail.com>
2023-01-30 21:11:44 +1300

    Tidy up API base URL

--- src/Application/Spotify/Constants.cs
+++ src/Application/Spotify/Constants.cs
@@ -2,6 +2,6 @@ namespace Application.Spotify;
 
 public class Constants
 {
-  public static readonly string API_BASE_URL = "https://api.spotify.com";
+  public static readonly string API_BASE_URL = "https://api.spotify.com/v1";
   public static readonly string ACCOUNTS_BASE_URL = "https://accounts.spotify.com";
 }
