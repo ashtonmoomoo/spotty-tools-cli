acec4e22742da767e941ef634cbafc89a5e0e1a3
Ashton Moore <ashton.chris.moore@gmail.com>
2023-02-01 21:13:15 +1300

    Move Dispatch into right place

--- src/Program.cs
+++ src/Program.cs
@@ -20,8 +20,7 @@ public class Program
       return 1;
     }
 
-    await Dispatch(args);
-    return 0;
+    return await Dispatch(args);
   }
 
   private static async Task<int> Dispatch(string[] args)
