34c4b96cf0b0791cac98bb6c529947c2002f6880
Ashton Moore <ashton.chris.moore@gmail.com>
2023-02-07 20:25:57 +1300

    Introduce required types

--- src/Application/Spotify/Responses/Album.cs
+++ src/Application/Spotify/Responses/Album.cs
@@ -9,3 +9,25 @@ public record class AlbumLite(
   [property: JsonPropertyName("artists")] Artist[] Artists,
   [property: JsonPropertyName("uri")] string URI
 );
+
+public record class AlbumTrack(
+  [property: JsonPropertyName("artists")] Artist[] Artists,
+  [property: JsonPropertyName("href")] string Href,
+  [property: JsonPropertyName("id")] string Id,
+  [property: JsonPropertyName("name")] string Name,
+  [property: JsonPropertyName("uri")] string URI
+);
+
+public record class Album(
+  [property: JsonPropertyName("href")] string Href,
+  [property: JsonPropertyName("id")] string Id,
+  [property: JsonPropertyName("name")] string Name,
+  [property: JsonPropertyName("artists")] Artist[] Artists,
+  [property: JsonPropertyName("uri")] string URI,
+  [property: JsonPropertyName("tracks")] Pagination<AlbumTrack> TracksPage
+);
+
+public record class AlbumWithAddedAt(
+  [property: JsonPropertyName("added_at")] DateTime AddedAt,
+  [property: JsonPropertyName("album")] Album Album
+);
